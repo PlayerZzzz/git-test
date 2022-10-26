4e330cb9ae8d0d1a7bc2f00b2e9063df518f174f
PlayerZzzz <116654745+PlayerZzzz@users.noreply.github.com>
2022-10-26 16:29:17 +0800

    Update GitTest.java

--- src/test/java/com/onefivezz/git/GitTest.java
+++ src/test/java/com/onefivezz/git/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("thrid commit");
         System.out.println("3 commit");
         System.out.println("44444444444444444444444444");
+         System.out.println("55555555555555555555555555555555555555");
     }
 }
