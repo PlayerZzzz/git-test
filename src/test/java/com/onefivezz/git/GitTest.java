1ed7ed30cbea504b0db98be3b7e6bb8f05223846
ofzz <ofzz@163.com>
2022-10-26 15:53:29 +0800

    git-test 2

--- src/test/java/com/onefivezz/git/GitTest.java
+++ src/test/java/com/onefivezz/git/GitTest.java
@@ -3,5 +3,6 @@ package com.onefivezz.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("thrid commit");
     }
 }
